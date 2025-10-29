f6fba95f44e3139ed7f582666443914619f82ce2
Vincent <alilingarcillasvincent@gmail.com>
2025-10-29 12:11:20 +0800

    added class stack with push and pop methods

--- infix_to_postfix.py
+++ infix_to_postfix.py
@@ -2,3 +2,23 @@ class Node:
     def __init__(self, data):
         self.data = 0
         self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        new_node = Node(data)
+        if self.top:
+            new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.top is None:
+            return None
+        else:
+            popped_node = self.top
+            self.top = self.top.next
+            popped_node.next = None
+            return popped_node.data
+        
